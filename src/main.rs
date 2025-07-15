d2005d354e37457889734d55c53dd3fa0fc3964e
rp-guruh <github.guruh@gmail.com>
2025-07-15 17:33:57 +0700

    [new] fitur get waktu shalat berdasarkan tgl hari ini dan id kota

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{error::Error, io::Write, fs::File, io::Result};
+use std::{error::Error, io::Write, fs::File, io::Result, thread, time::Duration};
 use serde::Deserialize;
-use slint::{ModelRc, SharedString, VecModel};
-use std::rc::Rc;
 
+use slint::{ModelRc, SharedString, VecModel, Timer};
+use std::rc::Rc;
 use std::io::{BufRead, BufReader};
 use std::collections::HashMap;
 use tokio::runtime::Runtime;
 use reqwest;
-use chrono::{Datelike, Local};
+use chrono::{Datelike, Local, Timelike};
+use misykat::hijri::HijriDate;
+use misykat::jiff;
+
+
 slint::include_modules!();
 
 // use std::{fs::File, io::BufReader};
@@ -50,7 +54,7 @@ fn search_city(locations: &[Lokasi], query: &str) -> Vec<Lokasi> {
 }
 
 
-fn save_settings_to_ini(path: &str, city_id: u32, city_name: &str, city_gmt: &str) -> Result<()> {
+fn save_settings_to_ini(path: &str, city_id: u32, city_name: &str, city_gmt: &str, jadwal_adzan: &WaktuAdzan) -> Result<()> {
     let mut file = File::create(path)?;
     writeln!(file, "[location]")?;
     writeln!(file, "id = {}", city_id)?;
@@ -59,11 +63,11 @@ fn save_settings_to_ini(path: &str, city_id: u32, city_name: &str, city_gmt: &st
 
 
     writeln!(file, "\n[adzan]")?;
-    writeln!(file, "shubuh = {}", city_id)?;
-    writeln!(file, "dzuhur = {}", city_name)?;
-    writeln!(file, "ashar = {}", city_gmt)?;
-    writeln!(file, "maghrib = {}", city_gmt)?;
-    writeln!(file, "isya = {}", city_gmt)?;
+    writeln!(file, "shubuh = {}", jadwal_adzan.shubuh)?;
+    writeln!(file, "dzuhur = {}", jadwal_adzan.dzuhur)?;
+    writeln!(file, "ashar = {}", jadwal_adzan.ashar)?;
+    writeln!(file, "maghrib = {}", jadwal_adzan.maghrib)?;
+    writeln!(file, "isya = {}", jadwal_adzan.isya)?;
 
     Ok(())
 }
@@ -86,7 +90,7 @@ fn read_location_settings(path: &str) -> std::io::Result<HashMap<String, String>
         let line = line.trim();
 
         if line.starts_with('[') && line.ends_with(']') {
-            in_location = &line[1..line.len()-1] == "location";
+            in_location = &line[1..line.len()-1] == "location" || &line[1..line.len()-1] == "adzan";
             continue;
         }
 
@@ -101,29 +105,90 @@ fn read_location_settings(path: &str) -> std::io::Result<HashMap<String, String>
     Ok(settings)
 }
 
-async fn get_waktu_adzan(day: &str, month: &str, year: &str) -> std::result::Result<String, Box<dyn Error>> {
-    let url = format!("https://adzan-indonesia-api.vercel.app/adzan?cityId=67&month={}&year={}&date={}", month, year, day);
-    let res = reqwest::get(url).await?;
-    let text = res.text().await?;
-    Ok(text)
+async fn get_waktu_adzan(
+    city_id: &str,
+    day: &str,
+    month: &str,
+    year: &str,
+) -> std::result::Result<WaktuAdzan, Box<dyn std::error::Error>> {
+    let url = format!(
+        "https://adzan-indonesia-api.vercel.app/adzan?cityId={}&month={}&year={}&date={}",
+        city_id, month, year, day
+    );
+
+    let res: serde_json::Value = reqwest::get(url).await?.json().await?;
+
+    let waktu_adzan = WaktuAdzan {
+        shubuh: res["data"]["data"]["adzan"]["shubuh"]
+            .as_str()
+            .unwrap_or("")
+            .to_string(),
+        dzuhur: res["data"]["data"]["adzan"]["dzuhur"]
+            .as_str()
+            .unwrap_or("")
+            .to_string(),
+        ashar: res["data"]["data"]["adzan"]["ashr"]
+            .as_str()
+            .unwrap_or("")
+            .to_string(),
+        maghrib: res["data"]["data"]["adzan"]["maghrib"]
+            .as_str()
+            .unwrap_or("")
+            .to_string(),
+        isya: res["data"]["data"]["adzan"]["isya"]
+            .as_str()
+            .unwrap_or("")
+            .to_string(),
+    };
+
+    Ok(waktu_adzan)
 }
+
 
 fn main() -> std::result::Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
-    // get waktu adzan
 
+    let weak_ui = ui.as_weak();
+
+    let timer = Timer::default();
+    timer.start(slint::TimerMode::Repeated, std::time::Duration::from_secs(1), move || {
+        if let Some(ui) = weak_ui.upgrade() {
+            let now = Local::now();
+            let jam = format!("{:02}:{:02}:{:02}", now.hour(), now.minute(), now.second());
+            ui.set_jam_saat_ini(SharedString::from(jam));
+        }
+    });
+
+    let date_gregorian = Local::now();
+    let year: i16 = date_gregorian.year() as i16;
+    let month: i8 = date_gregorian.month() as i8;
+    let month_name = date_gregorian.format("%B").to_string();
+    let day: i8 = date_gregorian.day() as i8;
+    let date = jiff::civil::date(year, month, day);
+
+    let hijr_date = HijriDate::from_gregorian(date, 0);
     
+
+    let formatted = format!("{} {} {} H - {} {} {}", hijr_date.day, hijr_date.month_english, hijr_date.year, day, month_name, year);
+    ui.set_tanggal_hari_ini(SharedString::from(formatted));
+
+
+
     match read_location_settings("data/settings.ini") {
         Ok(settings) => {
-            println!("ID: {}", settings.get("id").unwrap_or(&"0".to_string()));
-            println!("Name: {}", settings.get("name").unwrap_or(&"Unknown".to_string()));
-            println!("GMT: {}", settings.get("gmt").unwrap_or(&"+0".to_string()));
             let prayer_location = format!(
-                "{}-{}",
+                "{} - {}",
                 settings.get("name").unwrap_or(&"Unknown".to_string()),
                 settings.get("gmt").unwrap_or(&"+0".to_string())
             );
+
+            
             ui.set_location(SharedString::from(prayer_location));
+            ui.set_adzan_ashar(SharedString::from(settings.get("ashar").unwrap_or(&"".to_string())));
+            ui.set_adzan_dzuhur(SharedString::from(settings.get("dzuhur").unwrap_or(&"".to_string())));
+            ui.set_adzan_isya(SharedString::from(settings.get("isya").unwrap_or(&"".to_string())));
+            ui.set_adzan_maghrib(SharedString::from(settings.get("maghrib").unwrap_or(&"".to_string())));
+            ui.set_adzan_shubuh(SharedString::from(settings.get("shubuh").unwrap_or(&"".to_string())));
         }
         Err(e) => {
             eprintln!("Gagal membaca settings: {}", e);
@@ -185,31 +250,37 @@ fn main() -> std::result::Result<(), Box<dyn Error>> {
                 let city_name = parts[1].to_string();
                 let city_gmt = parts[2].to_string();
 
-                save_settings_to_ini("data/settings.ini", city_id, &city_name, &city_gmt)
-                    .expect("Failed to save settings");
 
-                show_message("Setting successfully saved!");
 
                 let now = Local::now();
                 let day = &now.day().to_string();
                 let month = &now.month().to_string();
                 let year = &now.year().to_string();
+                let id_city = &city_id.to_string();
                 let rt = Runtime::new().expect("Failed to create runtime");
                 rt.block_on(async {
-                    match get_waktu_adzan(day, month, year).await {
-                        Ok(text) => println!("Response:\n{}", text),
-                        Err(e) => eprintln!("Gagal: {}", e),
+                    let waktu_adzan = get_waktu_adzan(id_city, day, month, year).await.expect("Failed to fetch prayer times");
+                  
+                    save_settings_to_ini("data/settings.ini", city_id, &city_name, &city_gmt, &waktu_adzan)
+                    .expect("Failed to save settings");
+                    show_message("Setting successfully saved!");
+                    if let Some(ui) = ui_weak.upgrade() {
+                        ui.set_location(SharedString::from(format!("{}-{}", city_name, city_gmt)));
+                        ui.set_adzan_shubuh(SharedString::from(waktu_adzan.shubuh));
+                        ui.set_adzan_dzuhur(SharedString::from(waktu_adzan.dzuhur));
+                        ui.set_adzan_ashar(SharedString::from(waktu_adzan.ashar));
+                        ui.set_adzan_maghrib(SharedString::from(waktu_adzan.maghrib));
+                        ui.set_adzan_isya(SharedString::from(waktu_adzan.isya));
                     }
                 });
 
-                if let Some(ui) = ui_weak.upgrade() {
-                    ui.set_location(SharedString::from(format!("{}-{}", city_name, city_gmt)));
-                }
             }
         });
 
         dialog.show().unwrap();
+        
     }
+    
 });
     ui.run()?;
     Ok(())
